51499f0630c568a15b7393e56ef87224db2166f8
Shivam Singh <shivamsinghbsr0@gmail.com>
2026-05-13 23:35:19 +0530

    Setting up the node server and connecting to database

---
+++ server/server.js
@@ -0,0 +1,10 @@
+import 'dotenv/config'
+import app from "./src/app.js";
+import { connectDb } from './src/config/db.js';
+
+
+connectDb();
+
+app.listen(3000, () => {
+    console.log("Server started.")
+})
